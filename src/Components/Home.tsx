c850c6efa19ed337cb876a9687e6bfe79827f527
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-12-11 14:39:28 +0100

    Update Home.tsx

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -18,7 +18,7 @@ const Home: React.FC = () => {
       .writeText(link)
       .then(() => setCopySuccess("Link copied!"))
       .catch(() => setCopySuccess("Failed to copy link"));
-    alert('Link copied');
+    alert(copysuccess, 'Link copied');
   };
 
   return (
