64d04c015b919410fbc5204a6ab74f8a54ce5e1c
Azeezbank <bankoleazeezb98@gmail.com>
2025-06-14 16:23:39 +0100

    updsated

--- src/Components/AdminPage/users/UserInfo.tsx
+++ src/Components/AdminPage/users/UserInfo.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import './UserInfo.css';
-import { s } from 'framer-motion/client';
 
 interface UserInfo {
     d_id: number,
